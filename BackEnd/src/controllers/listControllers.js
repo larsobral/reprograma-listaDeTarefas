559a0a1d99a388c6c85eae935db606c0b9b25296
Larissa Vilela Sobral <larissavilelasobral@gmail.com>
2021-09-02 01:33:16 -0300

    adicionado novas rotas

--- BackEnd/src/controllers/listControllers.js
+++ BackEnd/src/controllers/listControllers.js
@@ -7,6 +7,28 @@ const getAll = async (req, res) => {
   res.json(cards)
 }
 
+// buscar card pelo id gerado pelo mongo
+const getCardById = async (req, res) => {
+  const resquestId = req.params.id
+  Lista.findOne({ _id: resquestId }, function (err, cardFound) {
+    if (err) {
+      res.status(500).send({ message: err.message })
+    } else {
+      if (cardFound) {
+        res.status(200).send(cardFound.toJSON())
+      } else {
+        res.status(204).send();
+      }
+    }
+  })
+};
+
+// const getAllvida = async (req, res) => {
+//   const cards = await Lista.find().populate('categoria')
+//   const cardsFiltrados = cards.filter(card => card.categoria == "vida")
+//   res.json(cardsFiltrados)
+// }
+
 // criar novo card
 const createCard = async (req, res) => {
   const card = new Lista({
@@ -27,18 +49,35 @@ const createCard = async (req, res) => {
   }
 }
 
-// const getAll = async (req, res) => {
-//   console.log('pegou todos')
-//   const card = await Cards.find().populate('list')
-//   console.log(card)
-//   try {
-//     res.status(201).json(card)
-//   }catch {
-//     res.status(500).json({message: 'Cards não encontrados tente novamente'})
-//   }
-// }
+const updateTexto = async (req, res) => {
+  try {
+    // tentar encontrar um card por id
+    const card = await Lista.findById(req.params.id)
+    // se não encontrar 
+    if (req.body.texto != null) {
+      card.texto = req.body.texto
+    }
+    const cardAtualizado = await card.save()
+    res.status(200).json(cardAtualizado)
+  } catch (err) {
+    res.status(500).json({message: err.message})
+  }
+};
+
+const deleteCard = async (req, res) => {
+  try {
+    const card = await Lista.findById(req.params.id)
+    const novaLista = await card.remove()
+    res.status(200).json(novaLista)
+  } catch (err) {
+    res.status(409).json({error: 'Card já foi deletado.'})
+  }
+}
 
 module.exports = {
   getAll,
-  createCard
+  createCard,
+  updateTexto,
+  getCardById,
+  deleteCard
 }
--- BackEnd/src/routes/listas.routes.js
+++ BackEnd/src/routes/listas.routes.js
@@ -6,13 +6,22 @@ const controller = require('../controllers/listcontrollers');
 // listar todos os cards
 router.get('/', controller.getAll)
 
+// lista card pelo id
+router.get('/:id', controller.getCardById)
+
+// lista card por categoria
+// router.get('/:id', controller.getCardByCategoria)
+
 // criar um novo card
 router.post('/create', controller.createCard)
 
+// altera o texto de um card por id
+router.patch('/update/:id', controller.updateTexto); 
+
 // router.patch('/:id', controller.updateStudio)
 
-// router.delete('/:id', controller.deleteStudio)
+router.delete('/delete/:id', controller.deleteCard)
 
-// router.get('/estudio', controller.nomeStudio)
+// router.get('/vida', controller.getAllvida)
 
 module.exports = router
