37b413e274f129bccd0777325c5553ad3c7fb24a
Larissa Vilela Sobral <larissavilelasobral@gmail.com>
2021-09-04 14:41:28 -0300

    add cors

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+
+// servidor
+const PORT = process.env.PORT;
 
 //conectar o db
 const db = require('./src/data/database')
 db.connect()
 
 //usar as rotas
+app.use(cors())
 app.use(express.json())
 
 // rota http://localhost:8080/todos sendo chamada com sucesso
 const listasRouter = require('./src/routes/listas.routes')
 app.use('/todos', listasRouter)
-
-// servidor
-const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
